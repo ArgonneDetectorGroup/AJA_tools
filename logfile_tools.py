ffd5c19a63d4f56502e96a2e8c1d7fd09d289c37
Faustin Carter <faustin.carter@gmail.com>
2017-02-15 18:16:07 -0600

    fix importing of datetimes so it works for AM/PM

--- logfile_tools.py
+++ logfile_tools.py
@@ -1,6 +1,7 @@
+import dateutil.parser as dp
 import pandas as pd
 
 def import_logfile(file_name):
     """Read in an AJA logfile to a pandas.DataFrame object indexed by datetime."""
-    df = pd.read_csv(file_name, sep='\t', parse_dates=[[0,1]], infer_datetime_format=True, index_col=0)
+    df = pd.read_csv(file_name, sep='\t', parse_dates=[[0,1]], date_parser=dp.parse, index_col=0)
     return df
